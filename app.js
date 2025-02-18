50e5b54f616c3f191e5f6395c7f4e1e7a0628f8d
thghost21 <tylerhacker21@gmail.com>
2025-02-18 15:00:44 -0700

    main page layout and images. need fine tuning. JS array done

--- app.js
+++ app.js
@@ -1,5 +1,19 @@
 //SECTION - Global
+const iceCream = [
+  { name: 'Cookie Dough', price: 4, quantity: 0, type: 'scoop' },
+  { name: 'Vanilla', price: 3, quantity: 0, type: 'scoop' },
+  { name: 'Strawberry', price: 4, quantity: 0, type: 'scoop' },
+  { name: 'Chocolate', price: 4, quantity: 0, type: 'scoop' },
+  { name: 'Birthday Cake', price: 3, quantity: 0, type: 'scoop' },
+  { name: 'Mint', price: 3, quantity: 0, type: 'scoop' },
+  { name: 'Sprinkles', price: 2, quantity: 0, type: 'topping' },
+  { name: 'Chocolate Chips', price: 3, quantity: 0, type: 'topping' },
+  { name: 'Gummy Worms', price: 2, quantity: 0, type: 'topping' },
+  { name: 'Waffle Cone', price: 3, quantity: 0, type: 'vessel' },
+  { name: 'Waffle Bowl', price: 4, quantity: 0, type: 'vessel' },
+  { name: 'Bowl', price: 2, quantity: 0, type: 'vessel' },
 
+]
 //!SECTION
 
 
